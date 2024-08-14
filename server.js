96b0d50b9fd126da0a27e941edeb0b89eed1286b
Philipe Da Motta Mello <38898462+Philipessj1@users.noreply.github.com>
2024-08-14 10:35:46 -0300

    feat: working with json

--- server.js
+++ server.js
@@ -10,15 +10,19 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-    
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+//  app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/about', (req, res) => {
-    
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+let posts = [
+    { id: 1, title: 'Post One' },
+    { id: 2, title: 'Post Two' },
+    { id: 3, title: 'Post Three' },
+];
+
+app.get('/api/posts', (req, res) => {
+
+    res.json(posts);
+})
+
 
 app.listen(PORT, () => {
 
