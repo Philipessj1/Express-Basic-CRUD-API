634a17b402fa4fa9241a5f866f24c58f076d5bb4
Philipe Da Motta Mello <38898462+Philipessj1@users.noreply.github.com>
2024-08-14 10:09:25 -0300

    feat: res.sendFile() Method

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const app = express();
 
@@ -6,7 +12,12 @@ const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     
-    res.send('<h1>Hello World!!</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(PORT, () => {
