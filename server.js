15b84fc8364154f8dbd349f85d0d8bceebf08963
Philipe Da Motta Mello <38898462+Philipessj1@users.noreply.github.com>
2024-08-14 09:52:14 -0300

    feat: simple server

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    
+    res.send('<h1>Hello World!!</h1>');
+});
+
+app.listen(PORT, () => {
+
+    console.log(`Server is Running on ${PORT}`);
+});
